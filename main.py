0b1360c9da08374f2ea5709214250ebdfc358974
zcimrn <zcimrn@ya.ru>
2026-06-08 00:27:54 +0400

    commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+import pykakasi
+import regex
+import wordfreq
+
+
+def is_hiragana(text: str) -> bool:
+    return regex.fullmatch(r"\p{Hiragana}+", text) is not None
+
+
+def main(*, word_length=6):
+    kakasi = pykakasi.kakasi()
+
+    word_set = set()
+
+    for ja_word in wordfreq.top_n_list(lang="ja", n=100000):
+        if not is_hiragana(ja_word):
+            continue
+
+        word = "".join(x["hepburn"] for x in kakasi.convert(ja_word))
+
+        if len(word) != word_length:
+            continue
+
+        word_set.add(word)
+
+    print(f"{len(word_set)} words found", file=sys.stderr)
+
+    print(random.choice(sorted(word_set)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main()
+
+    else:
+        main(word_length=int(sys.argv[1]))
